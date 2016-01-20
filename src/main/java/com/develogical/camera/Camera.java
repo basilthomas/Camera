61c47cf53a88825a702346f647768be7a61b94ac
a@b <a@b>
2016-01-20 10:12:36 +0000

    first test pass

--- src/main/java/com/develogical/camera/Camera.java
+++ src/main/java/com/develogical/camera/Camera.java
@@ -2,12 +2,22 @@ package com.develogical.camera;
 
 public class Camera {
 
+
+    private Sensor sense;
+
+    public Camera(Sensor sense) {
+
+        this.sense = sense;
+    }
+
     public void pressShutter() {
         // not implemented
     }
 
     public void powerOn() {
         // not implemented
+
+        sense.powerUp();
     }
 
     public void powerOff() {
--- src/test/java/com/develogical/camera/CameraTest.java
+++ src/test/java/com/develogical/camera/CameraTest.java
@@ -1,8 +1,13 @@
 package com.develogical.camera;
 
+import org.jmock.Expectations;
+import org.jmock.Mockery;
+import org.jmock.api.Expectation;
 import org.jmock.integration.junit4.JUnitRuleMockery;
 import org.junit.Rule;
 import org.junit.Test;
+
+import static org.jmock.internal.Cardinality.exactly;
 
 public class CameraTest {
 
@@ -12,7 +17,23 @@ public class CameraTest {
     @Test
     public void switchingTheCameraOnPowersUpTheSensor() {
 
+        final Sensor sense = context.mock(Sensor.class);
+
          // write your test here
+        Camera cam = new Camera(sense);
+
+
+
+
+
+        context.checking(new Expectations() {{
+        exactly(1).of(sense).powerUp();
+
+        }});
+
+        cam.powerOn();
     }
+
+
 
 }
